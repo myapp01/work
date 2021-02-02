2ccb2827af96ca87d6d9aab0b48fa2c7c1832d30
myapp01 <69285962+myapp01@users.noreply.github.com>
2021-02-02 11:24:45 +0700

    Add files via upload

---
+++ condition201.js
@@ -0,0 +1,9 @@
+const n = 12;
+const a = 2
+for(let i = 1;i<=n;i++)
+{
+
+    let x = console.log(m +'x' + i + ' = '+ i*m);
+    
+    document.getElementById("display").innerHTML ="x";
+}
---
+++ condition202.js
@@ -0,0 +1,8 @@
+let i = 1;
+while(i<3)
+{
+    //console.log ('I love Earth');
+    //console.log("1 x " + i +  " = " +i*1);
+    console.log(m + " x " + i + " = " +i*m);
+    i++; //i = 1+1
+}
